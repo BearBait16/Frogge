f5e7724d5ac8db549f7a4bf686763b91208864ed
Willpatbarr <145700970+Willpatbarr@users.noreply.github.com>
2024-10-18 17:56:16 -0600

    created iOS app project folder

---
+++ FrogApp/FrogApp/FrogAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrogAppApp.swift
+//  FrogApp
+//
+//  Created by William Barr on 10/18/24.
+//
+
+import SwiftUI
+
+@main
+struct FrogAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FrogApp/FrogAppUITests/FrogAppUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  FrogAppUITestsLaunchTests.swift
+//  FrogAppUITests
+//
+//  Created by William Barr on 10/18/24.
+//
+
+import XCTest
+
+final class FrogAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
