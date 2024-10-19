18947ba6b578efef6aaad53a0707834d4ee5915e
Jackson Aden <124087372+BearBait16@users.noreply.github.com>
2024-10-19 05:41:25 -0600

    Task: Added Jumping Frog

--- python/mongodb.py
+++ python/mongodb.py
@@ -28,7 +28,7 @@ def download_gif(file_id, download_path):
         print(f'Downloaded file to {download_path}')
 
 # Example usage
-file_path = 'Tree_Frog_Still.png'  # Ensure the path is correct
-relative_path = 'TreeFrogImage'  # Change this to your desired relative path
+file_path = 'jump_frog.gif'  # Ensure the path is correct
+relative_path = 'DartFrogImage'  # Change this to your desired relative path
 file_id = upload_gif(file_path, relative_path)  # Ensure the path is correct
 download_gif(file_id, 'downloaded_file.gif')  # Ensure the path is correct
